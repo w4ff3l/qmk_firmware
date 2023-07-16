564cc4b714291bca1e4016230c83120d653f4756
W4ff3l <w4ff3l@gmail.com>
2023-07-16 15:57:21 +0200

    update layout; add visual layout in comment

--- keyboards/ymdk/ymd40/air40/keymaps/w4ff3l/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/w4ff3l/keymap.c
@@ -1,4 +1,5 @@
-/* Copyright 2022 Dennis Kruyt (dennis@kruyt.org)
+/*
+ * Copyright 2023
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,72 +16,101 @@
  */
 
 #include QMK_KEYBOARD_H
+#include "keymap.h"
 
 enum custom_keycodes {
-    LHYPER_C_T = SAFE_RANGE,
-    RHYPER_C_T,
-    LALT_C_T,
-    RALT_C_T,
+    LALT_LT = SAFE_RANGE,
+    RALT_GT,
     RAISE,
     LOWER
 };
 
 enum layer_name {
-    _BASE_COLEMAK,
-    _BASE_QWERTY,
-    _RAISE,
+    _BASE_COLEMAK_DH,
     _LOWER,
-    _RGB
+    _RAISE,
+    _SPEC
 };
 
 void custom_mod_t(bool pressed, uint16_t key, char key_tap);
 void update_layer_state(bool pressed, enum layer_name layer);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE_COLEMAK] = LAYOUT_ortho_4x12_1x2uC(
-        KC_TAB,         KC_Q,       KC_W,     KC_F,    KC_P,       KC_G,   KC_J, KC_L,       KC_U,    KC_Y,     KC_SEMICOLON, KC_QUOT,
-        LCTL_T(KC_ESC), KC_A,       KC_R,     KC_S,    KC_T,       KC_D,   KC_H, KC_N,       KC_E,    KC_I,     KC_O,         RCTL_T(KC_ENT),
-        SC_LSPO,        KC_Z,       KC_X,     KC_C,    KC_V,       KC_B,   KC_K, KC_M,       KC_COMM, KC_DOT,   KC_SLSH,      SC_RSPC,
-        KC_LBRC,        LHYPER_C_T, LALT_C_T, KC_LGUI, RAISE,         KC_SPC,    LOWER,      KC_RGUI, RALT_C_T, RHYPER_C_T,   KC_RBRC
+    /* Base layer (Colemak-DH)
+     *                  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+     *                  | tab │  Q  │  W  │  F  │  P  │  B  │  J  │  L  │  U  │  Y  │  ;  │  '  │
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * Tap for 'Esc' -- │ esc │  A  │  R  │  S  │  T  │  G  │  M  │  N  │  E  │  I  │  O  │ ent │
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * Tap for  '('  -- │  ⇧  │  Z  │  X  │  C  │  D  │  V  │  K  │  H  │  ,  │  .  │  /  │  ⇧  │ -- Tap for ')'
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
+     *                  │  {  │ Hyp │ Alt │ GUI │  ↓  │   Space   │  ↑  │ GUI │ Alt │ Hyp │  }  │
+     *                  └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
+     *                          /                                                     /
+     * Tap for '[' and ']' ----'-----------------------------------------------------'
+     */
+    [_BASE_COLEMAK_DH] = LAYOUT_ortho_4x12_1x2uC(
+        KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,  KC_B,   KC_J, KC_L,  KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
+        CTL_ESC, KC_A,    KC_R,    KC_S,    KC_T,  KC_G,   KC_M, KC_N,  KC_E,    KC_I,    KC_O,    CTL_ENT,
+        SC_LSPO, KC_Z,    KC_X,    KC_C,    KC_D,  KC_V,   KC_K, KC_H,  KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
+        KC_LCBR, HYPER_L, LALT_LT, KC_LGUI, LOWER,    KC_SPC,    RAISE, KC_RGUI, RALT_GT, HYPER_R, KC_RCBR
         ),
-    [_BASE_QWERTY] = LAYOUT_ortho_4x12_1x2uC(
-        KC_TAB,         KC_Q,       KC_W,     KC_E,    KC_R,       KC_T,   KC_Y, KC_U,       KC_I,    KC_O,     KC_P,       KC_QUOT,
-        LCTL_T(KC_ESC), KC_A,       KC_S,     KC_D,    KC_F,       KC_G,   KC_H, KC_J,       KC_K,    KC_L,     KC_SCLN,    RCTL_T(KC_ENT),
-        SC_LSPO,        KC_Z,       KC_X,     KC_C,    KC_V,       KC_B,   KC_N, KC_M,       KC_COMM, KC_DOT,   KC_SLSH,    SC_RSPC,
-        KC_LBRC,        LHYPER_C_T, LALT_C_T, KC_LGUI, RAISE,          KC_SPC,   LOWER,      KC_RGUI, RALT_C_T, RHYPER_C_T, KC_RBRC
-        ),
-    [_RAISE] = LAYOUT_ortho_4x12_1x2uC(
-        KC_GRV,         KC_F1,       KC_F2,     KC_F3,   KC_F4,        KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,    KC_F10,     KC_QUOT,
-        LCTL_T(KC_ESC), KC_1,        KC_2,      KC_3,    KC_4,         KC_5,    KC_6,    KC_7,     KC_8,    KC_9,     KC_0,       RCTL_T(KC_ENT),
-        SC_LSPO,        KC_MINUS,    KC_EQUAL,  KC_GRV,  KC_BACKSLASH, _______, _______, _______,  _______, _______,  _______,    SC_RSPC,
-        KC_LBRC,        LHYPER_C_T,  LALT_C_T,  KC_LGUI, RAISE,            KC_BSPC,      LOWER,    KC_RGUI, RALT_C_T, RHYPER_C_T, KC_RBRC
-        ),
+
+    /* Number layer
+     *                  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+     *                  | tab │ F1  │ F2  │ F3  │ F4  │ F5  │ F6  │ F7  │ F8  │ F9  │ F10 │  '  │
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * Tap for 'Esc' -- │ esc │  1  │  2  │  3  │  4  │  5  │  6  │  7  │  8  │  9  │  0  │ ent │
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * Tap for  '('  -- │  ⇧  │  -  │  =  │  `  │  \  │  :  │ nda │ mda │  ,  │  .  │  /  │  ⇧  │ -- Tap for ')'
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
+     *                  │  {  │ Hyp │ Alt │ GUI │  ↓  │ Backspace │  ↑  │ GUI │ Alt │ Hyp │  }  │
+     *                  └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
+     *                          /                                                     /
+     * Tap for '[' and ']' ----'-----------------------------------------------------'
+     */
     [_LOWER] = LAYOUT_ortho_4x12_1x2uC(
-        KC_GRV,         KC_F11,      KC_F12,    KC_F13,  KC_F14,    KC_F15,  KC_F16,  KC_F17,   KC_F18,  KC_F19,   KC_F20,     KC_QUOT,
-        LCTL_T(KC_ESC), KC_EXLM,     KC_AT,     KC_HASH, KC_DOLLAR, KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_QUOT,  KC_DQUO,    RCTL_T(KC_ENT),
-        SC_LSPO,        KC_UNDS,     KC_PLUS,   KC_TILD, _______,   _______, _______, _______,  _______, _______,  _______,    SC_RSPC,
-        KC_LBRC,        LHYPER_C_T,  LALT_C_T,  KC_LGUI, RAISE,         KC_DELETE,    LOWER,    KC_RGUI, RALT_C_T, RHYPER_C_T, KC_RBRC
+        KC_TAB,  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_QUOT,
+        CTL_ESC, KC_1,    KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,    KC_0,    CTL_ENT,
+        SC_LSPO, KC_MINS, KC_EQUAL, KC_GRV,  KC_BSLS, KC_COLN, KC_NDSH, KC_MDSH,  KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
+        KC_LCBR, HYPER_L, LALT_LT,  KC_LGUI, LOWER,       KC_BSPC,      RAISE,    KC_RGUI, RALT_GT, HYPER_R, KC_RCBR
         ),
-    [_RGB] = LAYOUT_ortho_4x12_1x2uC(
-        _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______,      _______,     _______, _______, _______, _______, _______
+
+    /* Symbol layer
+     *                  ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
+     *                  | tab │ F11 │ F12 │ F13 │ F14 │ F15 │ F16 │ F17 │ F18 │ F19 │ F20 │  '  │
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * Tap for 'Esc' -- │ esc │  !  │  @  │  #  │  $  │  %  │  ^  │  &  │  *  │  '  │  "  │ ent │
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤
+     * Tap for  '('  -- │  ⇧  │  _  │  +  │  ~  │  |  │  :  │ nda │ mda │  ,  │  .  │  /  │  ⇧  │ -- Tap for ')'
+     *                  ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
+     *                  │  {  │ Hyp │ Alt │ GUI │  ↓  │   Delete  │  ↑  │ GUI │ Alt │ Hyp │  }  │
+     *                  └─────┴─────┴─────┴─────┴─────┴───────────┴─────┴─────┴─────┴─────┴─────┘
+     *                          /                                                     /
+     * Tap for '[' and ']' ----'-----------------------------------------------------'
+     */
+    [_RAISE] = LAYOUT_ortho_4x12_1x2uC(
+        KC_GRV,  KC_F11,  KC_F12,  KC_F13,  KC_F14,    KC_F15,  KC_F16,  KC_F17,   KC_F18,  KC_F19,  KC_F20,  KC_QUOT,
+        CTL_ESC, KC_EXLM, KC_AT,   KC_HASH, KC_DOLLAR, KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_QUOT, KC_DQUO, CTL_ENT,
+        SC_LSPO, KC_UNDS, KC_PLUS, KC_TILD, KC_PIPE,   KC_COLN, KC_NDSH, KC_MDSH,  KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC,
+        KC_LCBR, HYPER_L, LALT_LT, KC_LGUI, LOWER,         KC_DELETE,    RAISE,    KC_RGUI, RALT_GT, HYPER_R, KC_RCBR
+        ),
+
+    /* Special layer */
+    [_SPEC] = LAYOUT_ortho_4x12_1x2uC(
+        _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI,  RGB_VAD, KC_DEL,
+        _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,
+        _______, _______, _______, _______, _______,      _______,     _______, _______, _______,  _______, _______
         )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LHYPER_C_T:
-            custom_mod_t(record->event.pressed, KC_HYPR, '{');
-            return false;
-        case RHYPER_C_T:
-            custom_mod_t(record->event.pressed, KC_HYPR, '}');
-            return false;
-        case LALT_C_T:
+        case LALT_LT:
             custom_mod_t(record->event.pressed, KC_LALT, '<');
             return false;
-        case RALT_C_T:
+        case RALT_GT:
             custom_mod_t(record->event.pressed, KC_RALT, '>');
             return false;
         case LOWER:
@@ -104,7 +134,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         case _RAISE:
             rgb_matrix_mode(RGB_MATRIX_CUSTOM_left_column_glow);
             break;
-        case _RGB:
+        case _SPEC:
             rgb_matrix_mode(RGB_MATRIX_CUSTOM_top_row_glow);
             break;
         default:
@@ -134,7 +164,7 @@ void update_layer_state(bool pressed, enum layer_name layer) {
     } else {
         layer_off(layer);
     }
-    update_tri_layer(_LOWER, _RAISE, _RGB);
+    update_tri_layer(_LOWER, _RAISE, _SPEC);
 }
 
 void keyboard_post_init_user(void) {
